f498c63c689a9271d8370daa6fd0eb1e1577ae4d
NuyoahCai <17701273014@163.com>
2026-08-27 22:26:01 +0800

    chore: 初始化二手交易平台项目

---
+++ vue/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="height: 80px; padding: 10px 0; line-height: 30px; color: #666; text-align: center">
+    <div>Copyright © 2024 - 2025 闲置物品流通网 版权所有</div>
+    <div>工商备案号：ICP2009887739  <span style="margin-left: 20px">联系电话：010-88998877</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {}
+  },
+  created() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
